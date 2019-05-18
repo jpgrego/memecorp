8a3741dd9c3bb23aaaee628ea4bfbf9d6369210e
João Grego <jpedro@grego.pt>
2019-05-18 21:26:57 +0100

    Second version

--- server.js
+++ server.js
@@ -11,10 +11,10 @@ const HOST = '0.0.0.0';
 const app = express();
 app.use(express.static(process.env.PWD + '/img'));
 app.get('/', (req, res) => {
-    // res.send('<h1>João Grego</h1>\n' + 
-    //          '<img src="https://i.redd.it/7ixggbfxtsmy.png">');
     res.send('<h1>João Grego</h1>\n' + 
-             '<img src="/security.png" />');
+             '<img src="/meme.png">');
+    // res.send('<h1>João Grego</h1>\n' + 
+    //          '<img src="/security.png" />');
 });
 
 app.listen(PORT, HOST);
